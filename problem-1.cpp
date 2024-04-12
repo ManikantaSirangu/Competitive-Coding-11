19117f0be85d7569c6ce801be82c5d85b0e8fff1
manikanta <87496228+ManikantaSirangu@users.noreply.github.com>
2024-04-11 21:49:21 -0700

    Create problem-1.cpp

---
+++ problem-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& token) {
+        stack<int> st;
+        for(int i = 0; i < token.size(); i++){
+            if(token[i] == "+"){
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.pop();
+                a = a + b;
+                st.push(a);
+            }else if(token[i] == "-"){
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.pop();
+                a = b - a;
+                st.push(a);
+            }else if(token[i] == "*"){
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.pop();
+                a = a * b;
+                st.push(a);
+            }else if(token[i] == "/"){
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.pop();
+                a = b / a;
+                st.push(a);
+            }
+            else{
+                st.push(stoi(token[i]));
+            }
+        }
+        return st.top();
+    }
+};
